937c5b11a89cdef544d02d1e2504d7729364a5d6
YongHun Lee <yosiyong@gmail.com>
2022-07-10 00:04:43 +0900

    component準備

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ComponentTestController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,4 +21,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
+Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
+
+require __DIR__ . '/auth.php';
