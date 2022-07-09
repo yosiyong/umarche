726d0fce9886e33ef81cd73835dab20be8041502
YongHun Lee <yosiyong@gmail.com>
2022-07-10 01:22:48 +0900

    componentslot

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,1 +1,6 @@
+<x-tests.app>
+    <x-slot name="header">
+        ヘッダー１
+    </x-slot>
 component-test1です。
+</x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,1 +1,6 @@
-component-test2です。
+<x-tests.app>
+    <x-slot name="header">
+        ヘッダー２
+    </x-slot>
+    component-test2です。
+</x-tests.app>
