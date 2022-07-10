b591afe4a69dbd5f2605ee3265cab7e63894a435
YongHun Lee <yosiyong@gmail.com>
2022-07-10 16:03:17 +0900

    componentCssクラス名指定

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,4 +5,5 @@
 component-test1です。
 <x-tests.card title="タイトル1" content="本文" :message="$message" />
 <x-tests.card title="タイトル2" />
+<x-tests.card title="CSS変更" class="bg-red-300" />
 </x-tests.app>
