026c114bc8e4bebd30e50e5336b9bdcf12f0d0da
YongHun Lee <yosiyong@gmail.com>
2022-07-24 01:09:39 +0900

    softDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -59,7 +59,9 @@ class OwnersController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        return redirect()->route('admin.owners.index')->with('message','オーナー登録を実施しました。');
+        return redirect()
+        ->route('admin.owners.index')
+        ->with(['message'=>'オーナー情報を登録しました。','status'=>'info']);
     }
 
     /**
@@ -105,7 +107,7 @@ class OwnersController extends Controller
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message','オーナー情報を更新しました。');
+        ->with(['message'=>'オーナー情報を更新しました。','status'=>'info']);
     }
 
     /**
@@ -116,6 +118,25 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        //指定IDのデータを削除
+        Owner::findOrFail($id)->delete();
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with(['message'=>'オーナー情報を削除しました。','status'=>'alert']);
+    }
+
+    public function expiredOwnerIndex(){
+        $expiredOwners = Owner::onlyTrashed()->get();
+        return view('admin.expired-owners',
+        compact('expiredOwners'));
+    }
+
+    public function expiredOwnerDestroy($id){
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete();
+
+        return redirect()
+        ->route('admin.expired-owners.index')
+        ->with(['message'=>'オーナー情報を完全に削除しました。','status'=>'alert']);
     }
 }
