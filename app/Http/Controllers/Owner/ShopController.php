6225df9ecf71a01db7c063ab9addabcf4a14ca5c
YongHun Lee <yosiyong@gmail.com>
2022-07-26 01:11:31 +0900

    UploadImageRequest

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use App\Models\Shop;
 use InterventionImage;
+use App\Http\Requests\UploadImageRequest;
 
 class ShopController extends Controller
 {
@@ -56,7 +57,7 @@ class ShopController extends Controller
         return view('owner.shops.edit',compact('shop'));
     }
 
-    public function update(Request $request, $id)
+    public function update(UploadImageRequest $request, $id)
     {
         $imageFile = $request->image; //一時保存
 
