f99a02bc485bb2dd89be74d0cacc68dea2be70cc
YongHun Lee <yosiyong@gmail.com>
2022-07-29 00:16:21 +0900

    image index

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -4,17 +4,43 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Image;
+use Illuminate\Support\Facades\Auth;
 
 class ImageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+
+            //アクセスURLからimageのid取得
+            $id = $request->route()->parameter('image');
+            if(!is_null($id)){ // null判定
+
+                //shopのIdに紐づくownerのIDを取得
+                $imagesOwnerId = Image::findOrFail($id)->owner->id;
+                $imageId = (int)$imagesOwnerId; // キャスト 文字列→数値に型変換
+
+                //ログイン中のImageのIdとアクセスOwnerIdと同じかチェック
+                if($imageId !== Auth::id()){ // 同じでなかったら
+                    abort(404); // 404画面表示
+                }
+            }
+
+            return $next($request);
+        });
+    }
+
     public function index()
     {
-        //
+        //ログイン中のImageのidに紐づくShopデータ取得
+        $images = Image::where('owner_id', Auth::id())
+        ->orderBy('updated_at', 'desc')
+        ->paginate(20);
+
+        return view('owner.images.index', compact('images'));
     }
 
     /**
--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Owner;
 
 class Image extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'owner_id',
+        'filename',
+    ];
+
+    public function owner()
+    {
+        return $this->belongsTo(Owner::class);
+    }
 }
