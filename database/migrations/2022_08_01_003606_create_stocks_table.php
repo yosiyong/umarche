2892d1593963b60d147df0d2f64015236374c5b4
YongHun Lee <yosiyong@gmail.com>
2022-08-01 02:01:22 +0900

    Stock初期設定

---
+++ database/migrations/2022_08_01_003606_create_stocks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t_stocks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->tinyInteger('type');
+            $table->Integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //Schema::dropIfExists('stocks');
+        Schema::dropIfExists('t_stocks');
+    }
+}
