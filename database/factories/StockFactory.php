2d332e1d8b2f417e8bbb15379f2c284b4412b500
YongHun Lee <yosiyong@gmail.com>
2022-08-07 00:21:43 +0900

    product factory

---
+++ database/factories/StockFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
+
+class StockFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'product_id' => Product::factory(),
+            'type' => $this->faker->numberBetween(1,2),
+            'quantity' => $this->faker->randomNumber,
+        ];
+    }
+}
