cbb2c42cfb58f73ee791afd846563280d5d3b660
YongHun Lee <yosiyong@gmail.com>
2022-09-11 00:58:28 +0900

    Log

--- app/Http/Controllers/Owner/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Owner/Auth/AuthenticatedSessionController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -30,7 +31,9 @@ class AuthenticatedSessionController extends Controller
     {
         $request->authenticate();
 
+        //session再発行
         $request->session()->regenerate();
+        Log::debug('owner', $request->session()->all());
 
         return redirect()->intended(RouteServiceProvider::OWNER_HOME);
     }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //ownerから始まるURLの場合
+        if (request()->is('owner')) {
+            //owner用のクッキーを使う
+            config(['session.cookie' => config('session.cookie_owner')]);
+        }
+
+        //adminから始まるURLの場合
+        if (request()->is('admin')) {
+            //admin用のクッキーを使う
+            config(['session.cookie' => config('session.cookie_admin')]);
+        }
     }
 }
